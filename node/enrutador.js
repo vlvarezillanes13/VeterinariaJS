51cd814f6dded7429f554f6955431d61f311a17b
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2021-11-09 17:22:08 -0300

    PUT - MODIFICAR MASCOTA

--- node/enrutador.js
+++ node/enrutador.js
@@ -22,7 +22,18 @@ module.exports = {
         post: (data, callback) => {
             global.recursos.mascotas.push(data.payload);
             callback(201, data.payload);
+        },
+        put: (data, callback) => {
+            if(data.indice){
+                if(global.recursos.mascotas[data.indice]){
+                    global.recursos.mascotas[data.indice] = data.payload;
+                    return callback(200, global.recursos.mascotas[data.indice]);
+                }
+
+                return callback(404, {mensaje: `mascota con indice ${data.indice} no encontrada`});
             }
+            callback(400, { mensaje: "indice no enviado" });
+        },
     },
 
     noEncontrada: (data, callback) => {
